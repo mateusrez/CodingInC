67db7c460ed0599709dcef328474dd5f10e97db8
mateusrezende <malurez27@gmail.com>
2022-12-01 13:33:55 -0300

    Update

---
+++ AEDS 1/r.c/revisaovariaveis.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main()
+{
+    /*
+    char variavelChar = 'a';
+    printf("%c\n", variavelChar);
+    scanf (" %c", %variavelChar);
+    printf("%c\n", variavelChar);
+    */
+
+    /*
+    bool variavelBool = true;
+    printf("%iln", , variavelBool);
+    /*
+
+    /*
+    int variavelint = 10;
+    printf("%i\n", variavelInt);
+    scanf ("%i", &variavelInt);
+    printf("%i\n", variavelInt);
+    */
+
+    /*
+    float variavelFloat = 10.10;
+    printf("%f\n", variavelFloat);
+    scanf("%f", &variavelFloat);
+    printf ("%f", variavelFloat);
+    */
+   
+    /*
+    double variavelDouble = 10.10;
+    printf("%\n", variavelDouble);
+    scanf("%lf", &variavelDouble);
+    printf ("%f\n", variavelDouble);
+    */
+
+    return 0;
+}
